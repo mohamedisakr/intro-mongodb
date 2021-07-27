e431b68677eb17635ed46481c0a96cd544764d3b
mohamedisakr <mohamedisakr@gmail.com>
2021-07-27 13:56:36 +0200

    implementing express server app exercises

--- exercises/app/index.js
+++ exercises/app/index.js
@@ -1,28 +1,40 @@
-const express = require('express')
-const morgan = require('morgan')
-const connect = require('../connect')
-const {json, urlencoded} = require('body-parser')
-const app = express()
-const Todo = require('./todo')
+const express = require("express");
+const morgan = require("morgan");
+const connect = require("../connect");
+const { json, urlencoded } = require("body-parser");
+const app = express();
+const Todo = require("./todo");
+const uri = "mongodb://localhost:27017/intro-mongodb-testing";
 
-app.use(morgan('dev'))
-app.use(urlencoded({extended: true}))
-app.use(json())
+app.use(morgan("dev"));
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
-app.get('/todo/:id', async (req, res) => {
-  const todoId = req.params.id
-})
+app.get("/", async (req, res) => {
+  res.status(200).json("Welcome to my app");
+});
 
-app.get('/todos', async (req, res) => {
+app.get("/todos/:id", async (req, res) => {
+  const todoId = req.params.id;
+  const noteToBeRead = await Todo.findById(todoId).lean().exec();
+  res.status(200).json(noteToBeRead);
+});
 
-})
+app.get("/todos", async (req, res) => {
+  const allTodos = await Todo.find({}).lean().exec();
+  res.status(200).json(allTodos);
+});
 
-app.post('/todo', async () => {
-  const todoToCreate = req.body.todo
-})
+app.post("/todos", async (req, res) => {
+  const todoToCreate = req.body; //.todo;
+  const savedTodo = await Todo.create(todoToCreate);
+  res.status(201).json(savedTodo);
+});
 
-connect(/**add mongo url here */)
-  .then(() => app.listen(4000, () => {
-    console.log('server on http://localhost:4000')
-  }))
-  .catch(e => console.error(e))
+connect(uri)
+  .then(() =>
+    app.listen(4000, () => {
+      console.log("server on http://localhost:4000");
+    })
+  )
+  .catch((e) => console.error(e));
