bec1ff8c52d209938e1050a97d3d446c36b17ca9
mohamedisakr <mohamedisakr@gmail.com>
2021-07-27 11:14:27 +0200

    implementing all test cases for hooks and virtual

--- exercises/hooks/org.js
+++ exercises/hooks/org.js
@@ -1,26 +1,38 @@
-const mongoose = require('mongoose')
-const Project = require('./project')
-const cdnUrl = 'https://cdn.adminapp.com'
+const mongoose = require("mongoose");
+const Project = require("./project");
+const cdnUrl = "https://cdn.adminapp.com";
 
 const orgSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   subscription: {
     status: {
       type: String,
       required: true,
-      default: ['active'],
-      enum: ['active', 'trialing', 'overdue', 'canceled']
+      default: "active",
+      enum: ["active", "trialing", "overdue", "canceled"],
     },
     last4: {
       type: Number,
       min: 4,
-      max: 4
-    }
-  }
-})
+      max: 4,
+    },
+  },
+});
 
-module.exports = mongoose.model('org', orgSchema)
+// add a post remove hook to the org schema that removes all projects associated with the org
+orgSchema.post("remove", async function (doc, next) {
+  if (doc) {
+    const projectsToBeDeleted = await Project.deleteMany({
+      org: doc._id,
+    });
+
+    console.log("Child delete result: ", projectsToBeDeleted);
+  }
+  next();
+});
+
+module.exports = mongoose.model("org", orgSchema);
